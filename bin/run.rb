a2b89f952ae26955be50799a593a175ab751e8ad
anickel101 <anson.nickel@gmail.com>
2020-08-11 16:35:47 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative 'environment'
 
-db = SQLite3::Database.new('../db/lab_database.db')
+db = SQLite3::Database.new('../lab_database.db')
 sql_runner = SQLRunner.new(db)
+sql = 
 
-sql_runner.execute_sql_file
+sql_runner.execute_sql()
